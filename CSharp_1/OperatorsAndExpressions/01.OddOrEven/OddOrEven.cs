eeb36176817c2d4d40b64ba4ba629fd370c701a4
Anton Simeonov <djnoreaga@gmail.com>
2017-08-04 15:38:55 +0300

    Beginning the hw

---
+++ CSharp_1/OperatorsAndExpressions/01.OddOrEven/OddOrEven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homework_01
+{
+    class homework_01
+    {
+        static void Main()
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int checkBit = inputNum & 1;
+            if (checkBit == 0)
+            {
+                Console.WriteLine("even " + inputNum);
+            }
+            else
+            {
+                Console.WriteLine("odd " + inputNum);
+            }
+        }
+    }
+}
