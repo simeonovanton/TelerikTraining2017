6318d93a143a37f18afadd4dbbb4167719dae8ec
Anton Simeonov <djnoreaga@gmail.com>
2017-08-04 15:56:55 +0300

    Second task completed

---
+++ CSharp_1/OperatorsAndExpressions/MoonGravity/MoonGravity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoonGravity
+{
+    class MoonGravity
+    {
+        static void Main()
+        {
+            float inputW = float.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0.000}", inputW * 0.17F );
+        }
+    }
+}
