8cd341f474b376ea8cca495bd1e54b590422a805
simeonovanton <djnoreaga@gmail.com>
2017-08-15 01:28:21 +0300

    new tasks resolved - to 09.

---
+++ CSharp_1/OperatorsAndExpressions/08.PrimeCheck/PrimeCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.PrimeCheck
+{
+    class PrimeCheck
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+
+            if (input <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= (Math.Sqrt(input) + 1); i++)
+                {
+                    if (input % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(isPrime.ToString().ToLower());
+        }
+    }
+}
---
+++ CSharp_1/OperatorsAndExpressions/09.Trapezoid/Trapezoid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Trapezoid
+{
+    class Trapezoid
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = (a + b) * h / 2;
+            Console.WriteLine("{0:F7}", area);
+        }
+    }
+}
