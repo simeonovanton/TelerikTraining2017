ae880377900c747fb7978fbfef482b7d322444f7
simeonovanton <djnoreaga@gmail.com>
2017-08-14 18:50:52 +0300

    Added task N7 - point in a circle

---
+++ CSharp_1/OperatorsAndExpressions/07.PointInACircle/PointInACircle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.PointInACircle
+{
+    class PointInACircle
+    {
+        static void Main()
+        {
+            float inputX = float.Parse(Console.ReadLine());
+            float inputY = float.Parse(Console.ReadLine());
+
+            float circleRadius = 2f;
+            float inputRadius = (float)Math.Sqrt(inputX * inputX + inputY * inputY);
+            if (inputRadius <= circleRadius)
+            {
+                Console.WriteLine("yes {0:0.00}", inputRadius);
+            }
+            else
+            {
+                Console.WriteLine("no {0:0.00}", inputRadius);
+            }
+        }
+    }
+}
