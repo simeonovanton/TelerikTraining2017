8872fc0044f9a6de11ddded16a0e7dc7b7b840ad
Anton Simeonov <djnoreaga@gmail.com>
2017-08-18 16:12:04 +0300

    hmm

--- CSharp_1/OperatorsAndExpressions/16.BitSwap_AnotherVar/BitSwap.cs
+++ CSharp_1/OperatorsAndExpressions/16.BitSwap_AnotherVar/BitSwap.cs
@@ -15,35 +15,46 @@ namespace _16.BitSwap_AnotherVar
             int q = int.Parse(Console.ReadLine());
             int k = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
-
+            uint input = n;
+            //Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
 
             uint maskP = 0;
             uint maskQ = 0;
-
 
             for (int i = p; i <= (p + k - 1); i++, q ++)
             {
                 maskP += (uint)Math.Pow(2, i);
                 maskQ += (uint)Math.Pow(2, q);
             }
-            Console.WriteLine(Convert.ToString(maskP, 2).PadLeft(32, '0'));
-            Console.WriteLine(Convert.ToString(maskQ, 2).PadLeft(32, '0'));
-
-
+            //Console.WriteLine(Convert.ToString(maskP, 2).PadLeft(32, '0'));
+            //Console.WriteLine(Convert.ToString(maskQ, 2).PadLeft(32, '0'));
 
             uint nBitsP = n & maskP;
             uint nBitsQ = n & maskQ;
+            //Console.WriteLine("Bits in P area:");
+            //Console.WriteLine(Convert.ToString(nBitsP, 2).PadLeft(32, '0'));
 
+            //Console.WriteLine("Bits in Q area:");
+            //Console.WriteLine(Convert.ToString(nBitsQ, 2).PadLeft(32, '0'));
+
+            //Reversing bits in P and Q area:
             nBitsP ^= nBitsQ;
             nBitsQ ^= nBitsP;
             nBitsP ^= nBitsQ;
+            //Console.WriteLine("Reversed bits in the P area:");
+            //Console.WriteLine(Convert.ToString(nBitsP, 2).PadLeft(32, '0'));
+            //Console.WriteLine("Reversed bits in the Q area:");
+            //Console.WriteLine(Convert.ToString(nBitsQ, 2).PadLeft(32, '0'));
+
             n = n & ~(maskP);
             n = n & ~(maskQ);
+
+            //Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+
             n = n | nBitsP;
             n = n | nBitsQ;
 
-            Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
+            //Console.WriteLine(Convert.ToString(n, 2).PadLeft(32, '0'));
 
             Console.WriteLine(n);
         }
---
+++ CSharp_1/OperatorsAndExpressions/18.BitSwapNewTry/BitSwap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.BitSwapNewTry
+{
+    class BitSwap
+    {
+        static void Main()
+        {
+            uint n = uint.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = p; i <= (p + k - 1); i++, q++)
+            {
+                uint mask = 1;
+                // Reading bits at position p and q
+                int bitP = (int)(n & (mask << i)) >> i;
+                int bitQ = (int)(n & (mask << q)) >> q;
+                // Cleaning positions at positions p and q to 0;
+                n = n & ~(mask << i);
+                n = n & ~(mask << q);
+                //Reversing bits in the number n
+                n = n | (uint)(bitP << q);
+                n = n | (uint)(bitQ << i);
+
+            }
+            Console.WriteLine(n);
+
+
+
+        }
+    }
+}
