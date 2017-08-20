0ba32286f9e0fe7486d468bd59bfaa822a3b5729
simeonovanton <djnoreaga@gmail.com>
2017-08-20 22:46:10 +0300

    Added snippets for Console.ReadLine(), Console.Read() and Console.Write(); Added some hw;

---
+++ CSharp_1/ConsoleInputAndOutput/01.SumOf3Numbers/SumOf3Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SumOf3Numbers
+{
+    class SumOf3Numbers
+    {
+        static void Main()
+        {
+            float a = Convert.ToSingle(Console.ReadLine());
+            float b = Convert.ToSingle(Console.ReadLine());
+            float c = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(a + b + c);
+        }
+    }
+}
---
+++ CSharp_1/ConsoleInputAndOutput/02.CompanyInfo/CompanyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompanyInfo
+{
+    class CompanyInfo
+    {
+        static void Main()
+        {
+            string companyName = Console.ReadLine();
+            string companyAddress = Console.ReadLine();
+            string companyPhone = Console.ReadLine();
+            string companyFax = Console.ReadLine();
+            string companyWebSite = Console.ReadLine();
+            string managerFirstName = Console.ReadLine();
+            string managerSecondName = Console.ReadLine();
+            string managerAge = Console.ReadLine();
+            string managerPhone = Console.ReadLine();
+
+            Console.WriteLine(companyName);
+            Console.WriteLine("Address: " + companyAddress);
+            Console.WriteLine("Tel. " + companyPhone);
+            Console.WriteLine("Fax: {0}", string.IsNullOrEmpty(companyFax) ? "(no fax)" : companyFax);
+            Console.WriteLine("Web site: " + companyWebSite);
+            Console.WriteLine("Manager: {0} {1} (age: {2}, tel. {3})", managerFirstName, managerSecondName, managerAge, managerPhone);
+        }
+    }
+}
