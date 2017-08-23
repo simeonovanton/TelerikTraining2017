0087e02dc7ee50028a544d4eaa374b86cfe79c9e
simeonovanton <djnoreaga@gmail.com>
2017-08-24 01:42:43 +0300

    hw - work on task 17. - Spiral Martix

--- CSharp_1/Loops/17.SpiralMatrix/Properties/AssemblyInfo.cs
+++ CSharp_1/Loops/17.SpiralMatrix/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("17.SpiralMatrix")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bf494eb4-4519-4283-bece-e0a8a0605a2f")]
+[assembly: Guid("665ddba8-4fb9-41a0-9bd8-e6ba6b34a2dc")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
--- CSharp_1/Loops/17.SpiralMatrix/SpiralMatrix.cs
+++ CSharp_1/Loops/17.SpiralMatrix/SpiralMatrix.cs
@@ -11,6 +11,86 @@ namespace _17.SpiralMatrix
         static void Main()
         {
             int n = int.Parse(Console.ReadLine());
+            int counterX_Left = 0;
+            int counterX_Right = 0;
+            int counterY_Up = 0;
+            int counterY_Down = 0;
+            int counter = 1;
+            int ii = 1;
+            int jj = 1;
+
+
+            Right:
+            for (int j = 1 + counterX_Left; j <= n - counterX_Right; j++)
+            {
+                if (counter == n * n + 1)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(j, ii);
+                jj = j;
+                Console.Write(counter);
+                counter++;
+                if (j == n - counterX_Right)
+                {
+                    counterX_Right++;
+                    goto Down;
+                }
+            }
+
+            Down:
+            for (int i = 1 + counterY_Up; i <= n - counterY_Down; i++)
+            {
+                if (counter == n * n + 1)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(jj, i);
+                ii = i;
+                Console.Write(counter);
+                counter++;
+                if (i == n - counterY_Down)
+                {
+                    counterY_Down++;
+                    goto Left;
+                }
+            }
+
+            Left:
+            for (int j = n - counterX_Right; j >= 1 + counterX_Left; j--)
+            {
+                if (counter == n * n + 1)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(j, ii);
+                jj = j;
+                Console.Write(counter);
+                counter++;
+                if (j == 1 + counterX_Left)
+                {
+                    counterX_Left++;
+                    goto Up;
+                }
+            }
+
+            Up:
+            for (int i = n - counterY_Down; i >= 1 + counterY_Up; i--)
+            {
+                if (counter == n * n + 1)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(jj, i);
+                ii = i;
+                Console.Write(counter);
+                counter++;
+                if (i == 1 + counterY_Up)
+                {
+                    counterY_Up++;
+                    goto Right;
+                }
+            }
 
         }
     }
