07aadb0445f34e43a572f39012228645ce6bc403
simeonovanton <djnoreaga@gmail.com>
2017-08-23 08:24:11 +0300

    hw

---
+++ CSharp_1/Loops/09.MatrixOfNumbers/MatrixOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.MatrixOfNumbers
+{
+    class MatrixOfNumbers
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j < i + n; j++)
+                {
+                    Console.Write("{0} ", j);
+                }
+                Console.WriteLine();
+            }
+        }
+        
+}
+}
