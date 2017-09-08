ca3edff02a35b9ad52c2f2ef49d2ad8b4c8cfc1a
Anton Simeonov <djnoreaga@gmail.com>
2017-09-08 17:20:50 +0300

    task 2 done

---
+++ ExamPrepare_2015/Task_1.3/BullCows.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._3
+{
+    class BullCows
+    {
+        static void Main()
+        {
+            int[] number = Console.ReadLine().Select(n => (Convert.ToInt32(n)) - 48).ToArray();
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (b == 3 && c != 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            int[] temp = new int[4];
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1 j < 9; j++)
+                {
+
+                    for (int k = 1; k < 9; k++)
+                    {
+                        for (int l = 1; l < 9; l++)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
