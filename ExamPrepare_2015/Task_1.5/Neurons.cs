f34688ebc6863f289532bc006fbc8084d51ce7b7
simeonovanton <djnoreaga@gmail.com>
2017-09-15 01:30:08 +0300

    Adding use of list in task 1.5 - Neurons

--- ExamPrepare_2015/Task_1.5/Neurons.cs
+++ ExamPrepare_2015/Task_1.5/Neurons.cs
@@ -10,22 +10,69 @@ namespace Task_1._5
     {
         static void Main()
         {
+            #region variant 1
+            //       long input = 0;
+            //
+            //       while (true)
+            //       {
+            //           input = long.Parse(Console.ReadLine());
+            //           if (input == -1)
+            //           {
+            //               break;
+            //           }
+            //
+            //           long result = 0;
+            //           int mostLeftIndex = -1;
+            //           int mostRightIndex = -1;
+            //
+            //           for (int i = 0; i < 32; i++)
+            //           {
+            //               long mask = 1 << i;
+            //               long checkFor_1 = (input & mask) >> i;
+            //
+            //               if (checkFor_1 == 1)
+            //               {
+            //                   if (mostRightIndex == -1)
+            //                   {
+            //                       mostRightIndex = i;
+            //                   }
+            //                   mostLeftIndex = i;
+            //               }
+            //           }
+            //
+            //           for (int j = mostRightIndex + 1; j < mostLeftIndex; j++)
+            //           {
+            //               long mask = 1 << j;
+            //
+            //               if ((mask & input) == 0)
+            //               {
+            //                   // set j index of result to 1
+            //                   result |= mask;
+            //               }
+            //           }
+            //           Console.WriteLine(result);
+            //       }
+            //
+            //       // Console.WriteLine(Convert.ToString(a | b, 2).PadLeft(32, '0'));
+            #endregion
 
-            // Console.WriteLine(Convert.ToString(a | b, 2).PadLeft(32, '0'));
-
-            long input = 0;
-            //  List<int> list = new List<int>();
-            //  List<int> output = new List<int>();
+            #region variant 2
+            List<long> list = new List<long>();
+            List<long> output = new List<long>();
 
             while (true)
             {
-                input = long.Parse(Console.ReadLine());
+                long input = long.Parse(Console.ReadLine());
                 if (input == -1)
                 {
                     break;
                 }
-                // list.Add(input);
 
+                list.Add(input);
+            }
+
+            foreach (long num in list)
+            {
                 long result = 0;
                 int mostLeftIndex = -1;
                 int mostRightIndex = -1;
@@ -33,7 +80,7 @@ namespace Task_1._5
                 for (int i = 0; i < 32; i++)
                 {
                     long mask = 1 << i;
-                    long checkFor_1 = (input & mask) >> i;
+                    long checkFor_1 = (num & mask) >> i;
 
                     if (checkFor_1 == 1)
                     {
@@ -44,65 +91,27 @@ namespace Task_1._5
                         mostLeftIndex = i;
                     }
                 }
-
                 for (int j = mostRightIndex + 1; j < mostLeftIndex; j++)
                 {
                     long mask = 1 << j;
-                    //Console.WriteLine(Convert.ToString(mask, 2).PadLeft(32, '0'));
-
-                    if ((mask & input) == 0)
+                    if ((num & mask) == 0)
                     {
                         // set j index of resut to 1
                         result |= mask;
                     }
-                    
-
 
                 }
+                output.Add(result);
+
+            }
+
+            foreach (int result in output)
+            {
                 Console.WriteLine(result);
             }
 
-            // foreach (int num in list)
-            // {
-            //     int result = 0;
-            //     int mostLeftIndex = -1;
-            //     int mostRightIndex = -1;
-            //
-            //     for (int i = 0; i < 32; i++)
-            //     {
-            //         int mask = 1 << i;
-            //         int checkFor_1 = (num & mask) >> 1;
-            //
-            //         if (checkFor_1 == 1)
-            //         {
-            //             if (mostRightIndex == -1)
-            //             {
-            //                 mostRightIndex = i;
-            //             }
-            //             mostLeftIndex = i;
-            //         }
-            //     }
-            //     for (int j = mostRightIndex + 1; j < mostLeftIndex; j++)
-            //     {
-            //         int mask = 1 << j;
-            //         Console.WriteLine(Convert.ToString(mask, 2).PadLeft(32, '0'));
-            //
-            //         if ((num & mask) == 0)
-            //         {
-            //             // set j index of resut to 1
-            //             result |= mask;
-            //         }
-            //         
-            //     }
-            //     output.Add(result);
-            //
-            // }
-            //
-            // foreach (int result in output)
-            // {
-            //     Console.WriteLine(Convert.ToString(result, 2).PadLeft(32, '0'));
-            // }
-            //
+            #endregion
+
         }
     }
 }
