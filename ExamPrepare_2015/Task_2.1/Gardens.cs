23633fcaba68d66d1b11bbb742f4172528b43620
Anton Simeonov <djnoreaga@gmail.com>
2017-09-15 17:22:19 +0300

    Task_2.1/Gardens.cs Task_2.2/SecretsOfNumbers.cs Done!

--- ExamPrepare_2015/Task_2.1/Gardens.cs
+++ ExamPrepare_2015/Task_2.1/Gardens.cs
@@ -10,6 +10,41 @@ namespace Task_2._1
     {
         static void Main()
         {
+            int tomatoS = int.Parse(Console.ReadLine());
+            int tomatoA = int.Parse(Console.ReadLine());
+            int cucumberS = int.Parse(Console.ReadLine());
+            int cucumberA = int.Parse(Console.ReadLine());
+            int potatoS = int.Parse(Console.ReadLine());
+            int potatoA = int.Parse(Console.ReadLine());
+            int carrotS = int.Parse(Console.ReadLine());
+            int carrotA = int.Parse(Console.ReadLine());
+            int cabbageS = int.Parse(Console.ReadLine());
+            int cabbageA = int.Parse(Console.ReadLine());
+            int beansS = int.Parse(Console.ReadLine());
+
+            double totalSeedsCost =
+                tomatoS * 0.5 +
+                cucumberS * 0.4 +
+                potatoS * 0.25 +
+                carrotS * 0.6 +
+                cabbageS * 0.3 +
+                beansS * 0.4;
+            Console.WriteLine("Total costs: {0:F2}", totalSeedsCost);
+
+            int remainingArea = 250 - tomatoA - cucumberA - potatoA - carrotA - cabbageA;
+
+            if (remainingArea < 0)
+            {
+                Console.WriteLine("Insufficient area");
+            }
+            else if (remainingArea == 0)
+            {
+                Console.WriteLine("No area for beans");
+            }
+            else
+            {
+                Console.WriteLine("Beans area: {0}", remainingArea);
+            }
 
         }
     }
---
+++ ExamPrepare_2015/Task_2.2/SecretsOfNumbers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Task_2._2
+{
+    class SecretsOfNumbers
+    {
+        static void Main()
+        {
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+            BigInteger input = n;
+            BigInteger secrSum = 0;
+            int position = 0;
+            while (n != 0)
+            {
+                int digit =Math.Abs((int)( n % 10));
+                position++;
+                n = n / 10;
+                if (position % 2 != 0)
+                {
+                    secrSum += digit * position * position;
+                }
+                else
+                {
+                    secrSum += digit * digit * position;
+                }
+            }
+
+            Console.WriteLine(secrSum);
+
+            if (secrSum % 10 == 0)
+            {
+                Console.WriteLine("{0} has no secret alpha-sequence", input);
+            }
+            else
+            {
+                int numOfLetters = (int)secrSum % 10;
+                int r = (int)secrSum % 26;
+                for (int i = 0; i < numOfLetters; i++)
+                {
+                    Console.Write((char)('A' + (r + i) % 26));
+                  //int index = r + i;
+                  //char Letter = (char)(index + 65);
+                  //  if (index + 65 > 90)
+                  //  {
+                  //      index = 0;
+                  //      for (int j = 0; j < numOfLetters - i; j++, index++)
+                  //      {
+                  //          Letter = (char)(index + 65);
+                  //          if (index > 25)
+                  //          {
+                  //              index = 0;
+                  //          }
+                  //          Console.Write(Letter);
+                  //
+                  //      }
+                  //      break;
+                  //  }
+                  //  Console.Write(Letter);
+                    
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
