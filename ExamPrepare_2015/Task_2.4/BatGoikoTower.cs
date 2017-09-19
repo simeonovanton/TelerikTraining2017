bc982c874b7d749de89c96b0ad51b2b5eb21a8fd
Anton Simeonov <djnoreaga@gmail.com>
2017-09-19 17:25:45 +0300

    Bat Goiko Tower - Done!

--- ExamPrepare_2015/Task_2.4/BatGoikoTower.cs
+++ ExamPrepare_2015/Task_2.4/BatGoikoTower.cs
@@ -12,6 +12,41 @@ namespace Task_2._4
         {
             int n = int.Parse(Console.ReadLine());
 
+            int update = 2;
+            int rowByDash = 1;
+            for (int i = 0; i < n; i++)
+            {
+                char middleChar = '.';
+
+                if (i == rowByDash)
+                {
+                    middleChar = '-';
+                    rowByDash += update;
+                    update++;
+                }
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    Console.Write('.');
+                }
+
+                Console.Write('/');
+
+                for (int j = 0; j < i * 2; j++)
+                {
+                        Console.Write(middleChar);
+                }
+                
+
+                Console.Write('\\');
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    Console.Write('.');
+                }
+
+                Console.WriteLine();
+            }
 
         }
     }
