5d7e296bb04687212a3070a3437e7560034de041
Anton Simeonov <djnoreaga@gmail.com>
2017-09-26 17:21:42 +0300

    Page Game

---
+++ ExamPrepare_2015/Task_2.6/PageGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._6
+{
+    class PageGame
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
