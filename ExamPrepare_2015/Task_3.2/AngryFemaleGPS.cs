46a1b22444f5d1dcdf51024613cd6c82badf4dc1
simeonovanton <djnoreaga@gmail.com>
2017-10-03 01:53:48 +0300

    Task_3.2/AngryFemaleGPS.cs - Done!

--- ExamPrepare_2015/Task_3.2/AngryFemaleGPS.cs
+++ ExamPrepare_2015/Task_3.2/AngryFemaleGPS.cs
@@ -10,7 +10,77 @@ namespace Task_3._2
     {
         static void Main()
         {
+            /*
+            string input = Console.ReadLine();
 
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            int[] digits = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                digits[i] = input[i] - '0';
+            }
+
+            for (int i = 0; i < digits.Length; i = i + 2)
+            {
+                sumEven += digits[i];
+            }
+            for (int i = 1; i < digits.Length; i = i + 2)
+            {
+                sumOdd += digits[i];
+            }
+
+            if (sumEven > sumOdd)
+            {
+                Console.WriteLine("right {0}", sumEven);
+            }
+            else if (sumEven < sumOdd)
+            {
+                Console.WriteLine("left {0}", sumOdd);
+            }
+            else if (sumOdd == sumEven)
+            {
+                Console.WriteLine("straight {0}", sumOdd);
+            }
+            */
+           
+            long n = long.Parse(Console.ReadLine());
+            if (n < 0)
+            {
+                n *= -1;
+            }
+
+            long sumOdd = 0;
+            long sumEven = 0;
+
+            while (n != 0)
+            {
+                int digit = (int)(n % 10);
+
+                if (digit % 2 == 0)
+                {
+                    sumEven += digit;
+                }
+                else
+                {
+                    sumOdd += digit;
+                }
+                n = n / 10;
+            }
+
+            if (sumEven > sumOdd)
+            {
+                Console.WriteLine("right {0}", sumEven);
+            }
+            else if (sumEven < sumOdd)
+            {
+                Console.WriteLine("left {0}", sumOdd);
+            }
+            else if (sumOdd == sumEven)
+            {
+                Console.WriteLine("straight {0}", sumOdd);
+            }
         }
     }
 }
