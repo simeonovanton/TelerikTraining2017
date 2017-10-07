418118d87bb9c86878eb008401f169bf5eb46d53
simeonovanton <djnoreaga@gmail.com>
2017-10-07 10:55:43 +0300

    Task_5.1 3-6-9 Done!

--- ExamPrepare_2015/Task_4.1/Program_3_6_9.cs
+++ ExamPrepare_2015/Task_4.1/Program_3_6_9.cs
@@ -10,11 +10,38 @@ namespace Task_4._1
     {
         static void Main()
         {
-            uint a = uint.Parse(Console.ReadLine());
-            uint b = uint.Parse(Console.ReadLine());
-            uint c = uint.Parse(Console.ReadLine());
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            long c = long.Parse(Console.ReadLine());
+            long ac = 0;
 
+            switch (b)
+            {
+                case 3:
+                    ac = a + c;
+                    break;
+                case 6:
+                    ac = a * c;
+                    break;
+                case 9:
+                    ac = a % c;
+                    break;
 
+                default:
+                    break;
+            }
+
+            long r = 0;
+            if (ac % 3 == 0)
+            {
+                r = ac / 3;
+            }
+            else
+            {
+                r = ac % 3;
+            }
+            Console.WriteLine(r);
+            Console.WriteLine(ac);
         }
     }
 }
