5cbe4ae4be35b873d5934978b029f31cae2d7bb7
Anton Simeonov <djnoreaga@gmail.com>
2017-10-09 16:29:59 +0300

    task 4.3

---
+++ ExamPrepare_2015/Task_4.2/TheHorror.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._2
+{
+    class TheHorror
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int digitsNumber = 0;
+            int digitsSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if ((input[i] - '0') >= 0 && (input[i] - '0') <= 9)
+                    {
+                        digitsSum += (input[i] - '0');
+                        digitsNumber++;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("{0} {1}", digitsNumber, digitsSum);
+        }
+    }
+}
---
+++ ExamPrepare_2015/Task_4.3/XExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._3
+{
+    class XExpression
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            double digitFirst = 0;
+            double digitSecond = 0;
+            double result;
+            string sign;
+
+            foreach (char symbol in input)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    digitFirst = symbol - '0';
+                }
+
+                if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/')
+                {
+
+                }
+            }
+        }
+    }
+}
