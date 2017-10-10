a3f961bfe069b1bbc2a3b19981ac26310fd8ad80
Anton Simeonov <djnoreaga@gmail.com>
2017-10-10 17:25:36 +0300

    Task_4.4  DimamondsTrolls - Done! Task_4.5 - Warhead - working on it

--- ExamPrepare_2015/Task_4.4/DiamondTrolls.cs
+++ ExamPrepare_2015/Task_4.4/DiamondTrolls.cs
@@ -10,29 +10,58 @@ namespace Task_4._4
     {
         static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-            int width = 2 * n + 1;
-            int height = 6 + ((n - 3) / 2) * 3;
-
-            for (int r = 0; r < height; r++)
-            {
-                for (int c = 0; c < width; c++)
-                {
-                    if (c >= ((width - n) / 2) && c < ((width - n) / 2 + n) && r == 0)
-                    {
+             int n = int.Parse(Console.ReadLine());
+             int width = 2 * n + 1;
+             int height = 6 + ((n - 3) / 2) * 3;
+            
+             for (int r = 0; r < height; r++)
+             {
+                 for (int c = 0; c < width; c++)
+                 {
+                     // top line
+                     if (c >= ((width - n) / 2) && c < ((width - n) / 2 + n) && r == 0)
+                     {
+                         Console.Write('*');
+                     }
+                     // horizontal line
+                     else if (r == ( n / 2 + 1))
+                     {
+                         Console.Write('*');
+                     }
+                     else if (c == n)
+                     {
+                         Console.Write('*');
+                     }
+                     // making lower \
+                     else if (r - c == n / 2 + 1)
+                     {
+                         Console.Write('*');
+                     }
+                     // making upper \
+                     else if (r - c == - ((n / 2) * 3 + (n % 2)))
+                     {
+                         Console.Write('*');
+                     }
+                     // making lower /
+                     else if (r + c == 3 * n - n / 2 )
+                     {
+                        //Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.Write('*');
+                        //Console.ForegroundColor = ConsoleColor.Gray;
                     }
-                    else if (r == ( n / 2 + 1))
-                    {
-                        Console.Write('*');
-                    }
-                    else
-                    {
-                        Console.Write('.');
-                    }
-                }
-                Console.WriteLine();
-            }
+                    // making upper /
+                    else if (r + c == n / 2 + n % 2)
+                     {
+                         Console.Write('*');
+                     }
+                     else
+                     {
+                         Console.Write('.');
+                     }
+                 }
+                 Console.WriteLine();
+             }
+            //Console.WriteLine(7 % 2);
         }
     }
 }
---
+++ ExamPrepare_2015/Task_4.5/Warhead.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._5
+{
+    class Warhead
+    {
+        static void Main()
+        {
+            Console.WriteLine('*');
+            Console.WriteLine('-');
+            Console.WriteLine('*');
+            Console.WriteLine('1');
+            Console.WriteLine("disarmed");
+
+        }
+    }
+}
